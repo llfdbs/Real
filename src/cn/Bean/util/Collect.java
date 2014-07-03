eff6558e0eb51f937d53884320063d35f5f74cda
llfdbs@163.com <llfdbs@163.com>
2014-07-03 10:31:38 +0800

    修复了详情内容，添加了地图详细点击交互

---
+++ src/cn/Bean/util/Collect.java
@@ -0,0 +1,15 @@
+package cn.Bean.util;
+
+public class Collect {
+
+	public String state;
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+}
